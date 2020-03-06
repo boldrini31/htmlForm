b084f52c9e175595cc949a396874fe5224b8443e
Mauro Boldrini <boldrini31@hotmail.com>
2020-03-06 17:48:48 -0300

    Feito as validacoes de alguns falta so telefone e celular que to tentando entender a mascara ainda

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
-# htmlForm
+# Primeiro Formulario HTML/CSS
 
-## Ajustar Readme
+## Estudos
+
+Esse formulario foi meu primeiro projeto basico feito para treinar minha iniciação em html e css juntando validações com Javascript
+
 
 ´´´
 https://www.makeareadme.com/
--- index.js
+++ index.js
@@ -1,4 +1,88 @@
-function foo(value) {
-  alert('haha ', value)
-  console.log("onsubmit ", value)
+function confirmEmail() {
+  const email = document.getElementById("email").value;
+  const conEmail = document.getElementById("conEmail").value;
+
+  if (email != conEmail) {
+    alert('Confirmação de email diferente')
+    document.getElementsById("conEmail").innerHTMl.style.border.color = "red";
+  }
+  else {
+    document.getElementsById("conEmail").style.border.color = "lightgray";
+  }
+}
+
+function confirmPass() {
+  const pass = document.getElementById("pass").value;
+  const conPass = document.getElementById("conPass").value;
+
+  if (pass != conPass) {
+    alert('Password e Confirmação diferentes')
+  }
+}
+
+function required() {
+  const empt = document.getElementsByClassName['required'].value;
+  if (empt == "") {
+    alert("Complete os campos obrigatorios em vermelho")
+    return false;
+  }
+}
+
+function validaCpf() {
+  const regraValida = document.getElementById("cpf").value;
+  const cpfValido = /^(([0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2})|([0-9]{11}))$/;
+  if (cpfValido.test(regraValida) == true) {
+    alert("CPF Valido");
+  }
+  else {
+    alert("CPF Inválido");
+  }
+}
+
+function fMasc(objeto, mascara) {
+  obj = objeto
+  masc = mascara
+  setTimeout("fMascEx()", 1)
+}
+
+function fMascEx() {
+  obj.value = masc(obj.value)
+}
+
+function mCPF(cpf) {
+  cpf = cpf.replace(/\D/g, "")
+  cpf = cpf.replace(/(\d{3})(\d)/, "$1.$2")
+  cpf = cpf.replace(/(\d{3})(\d)/, "$1.$2")
+  cpf = cpf.replace(/(\d{3})(\d{1,2})$/, "$1-$2")
+  return cpf
+}
+
+function validaSobrenome() {
+  const sobrenome = document.getElementById("snome").value;
+  const padrao = /[^a-zà-ú]/gi;
+  const valida_sobrenome = sobrenome.match(padrao);
+
+  if (valida_sobrenome || !sobrenome) {
+    alert("Sobrenome contem caracteres invalidos ou é vazio")
+  }
+  else {
+    console.log("Sobrenome OK!")
+  }
+}
+
+function validaNome() {
+  const nome = document.getElementById("nome").value;
+
+  const padrao = /[^a-zà-ú]/gi;
+
+  const valida_nome = nome.match(padrao);
+
+
+  if (valida_nome || !nome) {
+    alert("Nome contem caracteres invalidos ou é vazio")
+  }
+  else {
+    console.log("Nome OK!")
+  }
+
 }
